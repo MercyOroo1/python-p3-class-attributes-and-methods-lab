a5e069e7d681ce6de118bf04500278bc2539c5cf
Mercy Oroo <mercy.oroo@student.moringaschool.com>
2024-06-02 10:32:13 +0300

    class attributes and methods lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,49 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+    def __init__ (self,name,artist,genre):
+        self.name = name 
+        self.artist = artist
+        self.genre = genre
+        self.add_to_count()
+        self.add_to_genre(self.genre)
+        self.add_to_artists(self.artist)
+        self.add_to_genre_count(self.genre)
+        self.add_to_artist_count(self.artist)
+
+    @classmethod
+    def add_to_count(cls):
+        Song.count += 1
+    
+    @classmethod
+    def add_to_genre(cls,genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+        else:
+            print ("Genre already exists")
+    
+    @classmethod
+    def add_to_artists(cls,artist):
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+        else:
+            print ("Artist already exists")
+    
+    @classmethod
+    def add_to_genre_count (cls,genre):
+        if genre in cls.genre_count:
+           cls.genre_count[genre] += 1
+        else:
+           cls.genre_count[genre] = 1
+    
+    @classmethod 
+    def add_to_artist_count (cls,artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] +=1
+        else:
+            cls.artist_count[artist] = 1
+           
+
